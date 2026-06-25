efcc39543ee62424dd4a4066c19e0d601e2a5d0e
Lejzer <89803550+Lento47@users.noreply.github.com>
2026-06-25 01:25:50 -0600

    feat(proof): export RunProof primitives

---
+++ packages/arcana/src/proof/index.ts
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT OR LicenseRef-arcana-Commercial
+// Copyright (c) 2026 arcana contributors
+
+export * from "./types.js"
+export * from "./create.js"
+export * from "./render.js"
+export * from "./store.js"
