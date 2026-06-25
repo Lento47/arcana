83d6df0cbac06758f01f0a4e1ec1d5bc45457036
Lejzer <89803550+Lento47@users.noreply.github.com>
2026-06-25 06:55:42 -0600

    feat(engine): export pipeline candidate security primitives

--- packages/engine/src/execution/index.ts
+++ packages/engine/src/execution/index.ts
@@ -2,4 +2,7 @@
 // Copyright (c) 2026 arcana contributors
 
 export * from "./action"
+export * from "./candidate"
+export * from "./pipeline"
 export * from "./risk"
+export * from "./security-context"
